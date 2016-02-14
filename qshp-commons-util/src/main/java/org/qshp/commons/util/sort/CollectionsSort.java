137598ed3dbd6c0bddad6deb935416155c0375e5
muyu <qinyong2020@sina.com>
2016-02-14 22:01:04 +0800

    增加改进冒泡排序算法

--- qshp-commons-util/src/main/java/org/qshp/commons/util/sort/CollectionsSort.java
+++ qshp-commons-util/src/main/java/org/qshp/commons/util/sort/CollectionsSort.java
@@ -55,10 +55,10 @@ public class CollectionsSort {
      * @param data
      */
     public static void bubbleSort(int data[]){
-        int n = data.length;
-        for(int i = 0 ; i< n-1; i++) {
+        int n = data.length-1;
+        for(int i = 0 ; i< n; i++) {
             // 右边最大值
-            for(int j = 0; j < n-i-1; j++) {
+            for(int j = 0; j < n-i; j++) {
                 if(data[j] > data[j+1]) {
                     swap(data,j,j+1);
                 }
@@ -69,6 +69,31 @@ public class CollectionsSort {
 //                    swap(data,i,j);
 //                }
 //            }
+        }
+
+    }
+
+    /**
+     * 改进后的冒泡排序
+     * 对冒泡排序常见的改进方法是加入一标志性变量exchange，
+     * 用于标志某一趟排序过程中是否有数据交换，如果进行某一趟排序时并没有进行数据交换，
+     * 则说明数据已经按要求排列好，可立即结束排序，避免不必要的比较过程。
+     * 改进算法：设置一标志性变量pos,用于记录每趟排序中最后一次进行交换的位置。
+     * 由于pos位置之后的记录均已交换到位,故在进行下一趟排序时只要扫描到pos位置即可。
+     * @param data
+     */
+    public static void bubbleSortForImprove(int data[]){
+        int i = data.length-1 ,pos;
+        while( i > 0){
+            pos = 0;
+            // 右边最大值
+            for(int j = 0; j < i; j++) {
+                if(data[j] > data[j+1]) {
+                    swap(data,j,j+1);
+                    pos = j;
+                }
+            }
+            i = pos;
         }
 
     }
@@ -87,9 +112,9 @@ public class CollectionsSort {
 
 
     public static void main(String[] args) {
-        int data[] = {1,3,7,5,9,8,3,0,1,2};
+        int data[] = {1,3,7,4,5,9,8,3,0,1,2};
 //        quickSort(data, 0, data.length-1);
-        bubbleSort(data);
+        bubbleSortForImprove(data);
         for (int i = 0; i < data.length; i++) {
             System.out.print(data[i] +",");
         }
